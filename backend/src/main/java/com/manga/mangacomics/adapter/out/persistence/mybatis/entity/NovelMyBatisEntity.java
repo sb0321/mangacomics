75854864ceca11ea0ad74676e2975d7b3c358623
sb0321 <woltaryksb1@gmail.com>
2025-05-17 19:29:53 +0000

    Novel 도메인 엔티티 수정

--- backend/src/main/java/com/manga/mangacomics/adapter/out/persistence/mybatis/entity/NovelMyBatisEntity.java
+++ backend/src/main/java/com/manga/mangacomics/adapter/out/persistence/mybatis/entity/NovelMyBatisEntity.java
@@ -2,4 +2,41 @@ package com.manga.mangacomics.adapter.out.persistence.mybatis.entity;
 
 public class NovelMyBatisEntity {
 
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
